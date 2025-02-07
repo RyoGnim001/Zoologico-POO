0127661740b31a7e4e38872f38602e5d590529ef
RyoGnim001 <105007789+RyoGnim001@users.noreply.github.com>
2025-02-07 02:48:06 -0300

    Update Zoologico.java

--- src/Zoologico.java
+++ src/Zoologico.java
@@ -1,36 +1,36 @@
 import java.util.ArrayList;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Zoologico {
-    private List<Animal> Animal = new ArrayList<>();
+    private List<Animal> animais = new ArrayList<>();
 
     public void adicionar(Animal animal){
-        Animal.add(animal);
+        animais.add(animal);
     }
 
     public void animarBicharada(){
-        for (Animal animal : Animal){
+        for (Animal animal : animais){
             animal.andar();
             animal.emitirSom();
         }
     }
 
     public void alimentarAnimais() {
-        for (Animal animal : Animal) {
+        for (Animal animal : animais) {
             animal.comer();
         }
     }
 
     public void listarAnimais() {
         System.out.println("\nAnimais no zoológico:");
-        for (Animal animal : Animal) {
+        for (Animal animal : animais) {
             System.out.println(animal.getClass().getSimpleName());
         }
     }
 
     public int totalAnimais() {
-        return Animal.size();
+        return animais.size();
     }
 }
+
 
